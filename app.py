e5658ebfacdeb27757a1dcd69b04ebec2e7f83da
belulok <belulok_sebastian@hotmail.com>
2025-02-24 22:51:09 +0800

    update port

--- app.py
+++ app.py
@@ -450,4 +450,5 @@ def match_resumes(job_id):
     return jsonify({"matches": results[:5]})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
